9d9691cdb98b016337dca1f9b313e2a3f1ab8131
Mark Okafor <okaformark@gmail.com>
2019-05-25 12:30:09 -0500

    add friend works

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import birfday from '../../components/birfday/birfday';
+import friends from '../../components/friends/friends';
 
 const authDiv = document.getElementById('auth');
 const birfdayDiv = document.getElementById('birfday');
@@ -18,6 +19,7 @@ const checkLoginStatus = () => {
       authNavbar.classList.add('hide');
       logoutNavbar.classList.remove('hide');
       birfday.birfdayDomStringBuilder(user.uid);
+      friends.showFriends();
     } else {
       authDiv.classList.remove('hide');
       birfdayDiv.classList.add('hide');
